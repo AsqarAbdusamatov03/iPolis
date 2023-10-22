9076cd9aa771c96919ad25576cd5a65a5a6709df
Suhrob7472 <baxtiyorov.suhrob.2002@gmail.com>
2023-10-22 23:21:16 +0500

    Add Pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./page/Home/Home.jsx";
 import Osago from "./page/Osago/Osago.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+import Contact from './page/Contact/Contact';
+import Companies from './page/Companies/Companies';
+import Single_companies from './page/Single_companies/Single_companies';
 
 
 function App() {
@@ -22,7 +25,9 @@ function App() {
         <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/osago" element={<Osago/>}/>
-            {/*<Route path="/contact" element={<Contact/>}/>*/}
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/insurance-companies" element={<Companies/>}/>
+            <Route path="/single-company" element={<Single_companies/>}/>
         </Routes>
         <Footer/>
     </>
