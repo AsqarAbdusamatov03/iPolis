2b87aaf7028f81065b838a485857e1e95ed04c49
AsqarAbdusamatov03 <asqarabdusamatov2@gmail.com>
2023-10-23 04:45:46 +0500

    Osago function All end full

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import {Route, Routes} from "react-router-dom";
 // import {useEffect} from "react";
 import Home from "./page/Home/Home.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-<<<<<<< HEAD
 import Osago from "./page/Osago/Osago.jsx";
 import {ToastContainer} from "react-toastify";
-=======
+
 import Contact from './page/Contact/Contact';
 import Companies from './page/Companies/Companies';
 import Single_companies from './page/Single_companies/Single_companies';
->>>>>>> 9076cd9aa771c96919ad25576cd5a65a5a6709df
+
 
 
 function App() {
@@ -30,16 +29,15 @@ function App() {
         <NavbarCom/>
         <Routes>
             <Route path="/" element={<Home/>} />
-<<<<<<< HEAD
+
             <Route path="/osago" element={<Osago/>  } />
             {/*<Route path="/contact" element={<Contact/>}/>*/}
 
-=======
             <Route path="/osago" element={<Osago/>}/>
             <Route path="/contact" element={<Contact/>}/>
             <Route path="/insurance-companies" element={<Companies/>}/>
             <Route path="/single-company" element={<Single_companies/>}/>
->>>>>>> 9076cd9aa771c96919ad25576cd5a65a5a6709df
+
         </Routes>
         <Footer/>
     </>
